f4c00a0e417b39b7fa46187c55b6c4e63e9fb381
root <root@pacificvps.(none)>
2014-04-19 02:54:19 +0100

    Added grunt script.

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+ grunt.initConfig({
+ pkg: grunt.file.readJSON('package.json'),
+copy: {
+      build: {
+        cwd: 'src',
+        src: [ '**' ],
+        dest: 'build',
+        expand: true
+      },
+    },
+  uglify: {
+    options: {
+      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    },
+    build: {
+      src: 'src/*.js',
+      dest: 'build/<%= pkg.name %>.min.js'
+    }
+  },
+   jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    imagemin: {                          
+    dynamic: {                         // Another target
+      files: [{
+        expand: true,                  // Enable dynamic expansion
+        cwd: 'src/',                   // Src matches are relative to this path
+        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+        dest: 'build/'                  // Destination path prefix
+      }]
+    }
+  }
+});
+
+grunt.loadNpmTasks('grunt-contrib-imagemin');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.registerTask('default', ['copy', 'jshint', 'uglify', 'imagemin']);
+
+
+}
+
