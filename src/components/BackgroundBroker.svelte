09693dec0ecc3c34547b6c303568ac313d11c48c
João Anes <joao.anes@gmail.com>
2021-01-21 17:09:09 +0000

    Add gpu-check before showing expensive bg

--- src/components/BackgroundBroker.svelte
+++ src/components/BackgroundBroker.svelte
@@ -1,10 +1,25 @@
 <script>
+  import { onMount } from "svelte";
+  import { getGPUTier } from "detect-gpu";
   import ThreeJsBackground from "./ThreeJSBackground.svelte";
   export let dorkMode;
 
-  let allowed = true;
+  let allowed = false;
+  onMount(async () => {
+    debugger;
+    const gpuTier = await getGPUTier();
+    allowed = gpuTier.tier >= 2;
+  });
 </script>
 
-{#if allowed}
-  <ThreeJsBackground {dorkMode} />
-{/if}
+<div class="loader" style="opacity: {allowed ? 1 : 0}">
+  {#if allowed}
+    <ThreeJsBackground {dorkMode} />
+  {/if}
+</div>
+
+<style>
+  .loader {
+    transition: opacity 3s;
+  }
+</style>
