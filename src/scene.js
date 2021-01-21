874f84b2cdc1fc8124d59f7163018d28fff774b6
João Anes <joao.anes@gmail.com>
2021-01-21 05:02:19 +0000

    Add preload, font swap

--- src/scene.js
+++ src/scene.js
@@ -4,7 +4,7 @@ let scene;
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
 const geometry = new THREE.PlaneGeometry(20, 20, 19, 19);
 for (var i = 0, l = geometry.vertices.length; i < l; i++) { //TODO EWWWWWWWWWW
-    geometry.vertices[i].z = Math.sin(Math.random() * 3.14) + Math.random();
+  geometry.vertices[i].z = Math.sin(Math.random() * 3.14) + Math.random();
 }
 let renderer;
 let main, counter = 0
@@ -14,20 +14,20 @@ export let uniforms = { 'dorkMode': { value: false }, 'uTime': { value: 1 } };
 const buildScene = () => {
 
 
-    var material = new THREE.MeshBasicMaterial({
-        color: uniforms['dorkMode'].value ? 0 : 0xffffff,
-    });
+  var material = new THREE.MeshBasicMaterial({
+    color: uniforms['dorkMode'].value ? 0 : 0xffffff,
+  });
 
 
-    geometry.computeFaceNormals();
-    geometry.computeVertexNormals();
+  geometry.computeFaceNormals();
+  geometry.computeVertexNormals();
 
 
 
-    const material2 = new THREE.ShaderMaterial({
+  const material2 = new THREE.ShaderMaterial({
 
-        uniforms: uniforms,
-        vertexShader: `attribute vec3 center;
+    uniforms: uniforms,
+    vertexShader: `attribute vec3 center;
     uniform float uTime;
     varying vec3 vCenter;
 
@@ -41,7 +41,7 @@ const buildScene = () => {
 
     }
 `,
-        fragmentShader: `
+    fragmentShader: `
     uniform bool dorkMode;
 
 			void main() {
@@ -50,22 +50,22 @@ const buildScene = () => {
 				gl_FragColor = dorkMode ? vec4(1.0,1.0,1.0,1.0) : vec4(0.0,0.0,0.0,1.0); ; 
 
 			}`,
-        side: THREE.DoubleSide,
+    side: THREE.DoubleSide,
 
 
-    });
+  });
 
-    var mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh)
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh)
 
-    // wireframe
-    var geo = new THREE.WireframeGeometry(mesh.geometry); // or WireframeGeometry
-    var wireframe = new THREE.LineSegments(geo, material2);
-    scene.add(wireframe);
+  // wireframe
+  var geo = new THREE.WireframeGeometry(mesh.geometry); // or WireframeGeometry
+  var wireframe = new THREE.LineSegments(geo, material2);
+  scene.add(wireframe);
 
 
-    camera.position.z = 2.5;
-    camera.rotation.x = 1.5;
+  camera.position.z = 2.5;
+  camera.rotation.x = 1.5;
 
 
 }
@@ -73,31 +73,31 @@ const buildScene = () => {
 let frame
 
 const animate = () => {
-    counter = (counter + 1) % 7
+  counter = (counter + 1) % 7
 
-    if (counter === 0) {
-        uniforms.uTime.value += 1
-    }
-    frame = requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+  if (counter === 0) {
+    uniforms.uTime.value += 1
+  }
+  frame = requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 };
 
 const resize = () => {
-    renderer.setSize(main.offsetWidth, main.offsetHeight)
-    camera.aspect = main.offsetWidth / main.offsetHeight;
-    camera.updateProjectionMatrix();
+  renderer.setSize(main.offsetWidth, main.offsetHeight)
+  camera.aspect = main.offsetWidth / main.offsetHeight;
+  camera.updateProjectionMatrix();
 };
 
 export const createScene = (el, dorkMode) => {
-    scene = new THREE.Scene();
-    renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el, alpha: true });
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el, alpha: true });
 
-    uniforms['dorkMode'].value = dorkMode
-    main = document.querySelector(".above-fold")
-    cancelAnimationFrame(frame)
-    buildScene()
-    resize();
-    animate();
+  uniforms['dorkMode'].value = dorkMode
+  main = document.querySelector(".above-fold")
+  cancelAnimationFrame(frame)
+  buildScene()
+  resize();
+  animate();
 }
 
 window.addEventListener('resize', resize);
