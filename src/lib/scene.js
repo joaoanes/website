5bf1681585a4ca97d32616ae7057da69b49bc2a6
João Anes <joao.anes@gmail.com>
2021-01-21 17:30:29 +0000

    Cleanup terrible late-night code

--- src/lib/scene.js
+++ src/lib/scene.js
@@ -1,84 +1,52 @@
 import * as THREE from 'three';
+export let uniforms = { 'dorkMode': { value: false }, 'delta': { value: 1 } };
 
 let scene;
-const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
-const geometry = new THREE.PlaneGeometry(20, 20, 19, 19);
-for (var i = 0, l = geometry.vertices.length; i < l; i++) { //TODO EWWWWWWWWWW
-  geometry.vertices[i].z = Math.sin(Math.random() * 3.14) + Math.random();
-}
 let renderer;
 let main, counter = 0
 
-export let uniforms = { 'dorkMode': { value: false }, 'uTime': { value: 1 } };
+let frame
+const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+const gridGeometry = new THREE.PlaneGeometry(20, 20, 19, 19);
+
 
 const buildScene = () => {
-
-
-  var material = new THREE.MeshBasicMaterial({
-    color: uniforms['dorkMode'].value ? 0 : 0xffffff,
-  });
-
-
-  geometry.computeFaceNormals();
-  geometry.computeVertexNormals();
-
-
-
-  const material2 = new THREE.ShaderMaterial({
-
-    uniforms: uniforms,
-    vertexShader: `attribute vec3 center;
-    uniform float uTime;
-    varying vec3 vCenter;
-
-
-    void main() {
-
-        vCenter = center;
-
-        gl_Position = projectionMatrix * modelViewMatrix * 
-            vec4( position.x , position.y ,  position.z + 0.1+ position.z * (sin(position.x + uTime / 10.0) / 30.0) , 1.0 ) ;
-
-    }
-`,
-    fragmentShader: `
-    uniform bool dorkMode;
-
-			void main() {
-
-
-				gl_FragColor = dorkMode ? vec4(1.0,1.0,1.0,1.0) : vec4(0.0,0.0,0.0,1.0); ; 
-
-			}`,
-    side: THREE.DoubleSide,
-
-
-  });
-
-  var mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh)
-
-  // wireframe
-  var geo = new THREE.WireframeGeometry(mesh.geometry); // or WireframeGeometry
-  var wireframe = new THREE.LineSegments(geo, material2);
-  scene.add(wireframe);
-
+  //TODO: Consider disposing of previous scene
+  gridGeometry.vertices = gridGeometry.vertices.map(
+    ({ x, y, z }) => ({ x, y, z: Math.sin(Math.random() * 3.14) + Math.random() })
+  )
 
   camera.position.z = 2.5;
   camera.rotation.x = 1.5;
 
+  var gridMaterial = new THREE.MeshBasicMaterial({
+    color: uniforms['dorkMode'].value ? 0 : 0xffffff,
+  });
 
+  const wireframeMaterial = new THREE.ShaderMaterial({
+
+    uniforms: uniforms,
+    vertexShader,
+    fragmentShader,
+    side: THREE.FrontSide,
+  });
+
+  var gridMesh = new THREE.Mesh(gridGeometry, gridMaterial);
+  scene.add(gridMesh)
+
+  var wireGeometry = new THREE.WireframeGeometry(gridGeometry);
+  var wireMesh = new THREE.LineSegments(wireGeometry, wireframeMaterial);
+  scene.add(wireMesh);
 }
 
-let frame
-
-const animate = () => {
+const renderLoop = () => {
   counter = (counter + 1) % 7
 
   if (counter === 0) {
-    uniforms.uTime.value += 1
+    uniforms.delta.value += 1
   }
-  frame = requestAnimationFrame(animate);
+
+  frame = requestAnimationFrame(renderLoop);
   renderer.render(scene, camera);
 };
 
@@ -88,16 +56,39 @@ const resize = () => {
   camera.updateProjectionMatrix();
 };
 
+// TODO
+// This probably could've been handed more sveltely, with 2way binding or something equally silly
 export const createScene = (el, dorkMode) => {
   scene = new THREE.Scene();
   renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el, alpha: true });
 
   uniforms['dorkMode'].value = dorkMode
   main = document.querySelector(".above-fold")
+
   cancelAnimationFrame(frame)
   buildScene()
   resize();
-  animate();
+  window.addEventListener('resize', resize);
+
+  renderLoop();
 }
 
-window.addEventListener('resize', resize);
+const vertexShader = `
+attribute vec3 center;
+uniform float delta;
+varying vec3 vCenter;
+
+void main() {
+    vCenter = center;
+    gl_Position = projectionMatrix * modelViewMatrix * 
+        vec4(position.x, position.y, position.z + 0.1 + position.z * (sin(position.x + delta / 10.0) / 30.0) , 1.0);
+}
+`
+
+const fragmentShader = `
+uniform bool dorkMode;
+
+void main() {
+  gl_FragColor = dorkMode ? vec4(1.0, 1.0, 1.0 ,1.0) : vec4(0.0, 0.0, 0.0, 1.0);
+}
+`
