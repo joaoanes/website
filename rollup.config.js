9185138da6983d5a483fbd65bc810d644e9d7feb
João Anes <joao.anes@gmail.com>
2021-01-31 00:02:37 +0000

    Replace tabs with spaces

--- rollup.config.js
+++ rollup.config.js
@@ -8,81 +8,81 @@ import css from 'rollup-plugin-css-only';
 const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
-	let server;
+  let server;
 
-	function toExit() {
-		if (server) server.kill(0);
-	}
+  function toExit() {
+    if (server) server.kill(0);
+  }
 
-	return {
-		writeBundle() {
-			if (server) return;
-			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
+  return {
+    writeBundle() {
+      if (server) return;
+      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      });
 
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
+      process.on('SIGTERM', toExit);
+      process.on('exit', toExit);
+    }
+  };
 }
 
 export default [
-	{
-		input: 'src/main.js',
-		output:
-		{
-			sourcemap: true,
-			format: 'esm',
-			dir: 'public/build/nm/'
-		},
-		plugins: [
-			svelte({
-				emitCss: false,
-				compilerOptions: {
-					legacy: true,
-					dev: !production
-				}
-			}),
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
-			production && terser(),
-		],
-		watch: {
-			clearScreen: false
-		}
-	},
-	{
-		input: 'src/main.js',
-		output:
-		{
-			sourcemap: true,
-			format: 'es',
-			dir: 'public/build/'
-		},
-		plugins: [
-			svelte({
-				compilerOptions: {
-					dev: !production
-				}
-			}),
-			css({ output: 'bundle.css' }),
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
+  {
+    input: 'src/main.js',
+    output:
+    {
+      sourcemap: true,
+      format: 'esm',
+      dir: 'public/build/nm/'
+    },
+    plugins: [
+      svelte({
+        emitCss: false,
+        compilerOptions: {
+          legacy: true,
+          dev: !production
+        }
+      }),
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
+      }),
+      commonjs(),
+      production && terser(),
+    ],
+    watch: {
+      clearScreen: false
+    }
+  },
+  {
+    input: 'src/main.js',
+    output:
+    {
+      sourcemap: true,
+      format: 'es',
+      dir: 'public/build/'
+    },
+    plugins: [
+      svelte({
+        compilerOptions: {
+          dev: !production
+        }
+      }),
+      css({ output: 'bundle.css' }),
+      resolve({
+        browser: true,
+        dedupe: ['svelte']
+      }),
+      commonjs(),
 
-			production && terser(),
-			!production && serve(),
-			!production && livereload('public'),
-		],
-		watch: {
-			clearScreen: false
-		}
-	},
+      production && terser(),
+      !production && serve(),
+      !production && livereload('public'),
+    ],
+    watch: {
+      clearScreen: false
+    }
+  },
 ]
