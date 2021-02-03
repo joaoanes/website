7d1a6d66b8e795cf1effbaeecca775283d1f3876
João Anes <joao.anes@gmail.com>
2021-02-03 21:35:53 +0000

    Change tabs to spaces

--- sapper-app/src/server.js
+++ sapper-app/src/server.js
@@ -1,3 +1,5 @@
+// Ignore this, I'm sure we don't use it.
+
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
@@ -7,11 +9,11 @@ const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+  .use(
+    compression({ threshold: 0 }),
+    sirv('static', { dev }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
